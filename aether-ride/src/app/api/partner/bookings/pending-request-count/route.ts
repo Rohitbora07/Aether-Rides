2105c190a91198650bc0a49a6a00ce03b0bf8a61
Rohitbora07 <rohitbora098@gmail.com>
2026-06-14 19:08:24 +0530

    feat: request couting api for driver rides

---
+++ aether-ride/src/app/api/partner/bookings/pending-request-count/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import  connectDB  from '@/lib/db'
+import { auth } from '@/lib/auth'
+import User from '@/models/user.model'
+import Booking from '@/models/booking.model'
+export async function GET(){
+    try{
+        await connectDB()
+        const session = await auth()
+        if( !session || !session.user?.email ){
+            return NextResponse.json({message:"Unauthorized"},
+                {status:400}
+            )
+        }
+        const partner = await User.findOne({ email:session.user.email })
+        if( !partner ){
+            return NextResponse.json({message:"User not found"},
+                {status:400}
+            )
+        }
+        const count = await Booking.countDocuments({
+            driver: partner.id,
+            bookingStatus: "requested"
+        })
+        return NextResponse.json({count}, {status:200})
+    }catch(error){
+        return NextResponse.json({message:`Pending request count error: ${error}`},
+            {status:500}
+        )
+    }
+}
